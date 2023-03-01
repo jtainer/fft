66f9964b54e6f6af3756328e2f97f29319a8bb98
jtain <jtain@debian>
2023-03-01 05:04:51 -0500

    Simplified recursive function calls to operate on a single buffer

--- fft.c
+++ fft.c
@@ -7,59 +7,67 @@
 #include "fft.h"
 #include <math.h>
 
-// Reads time domain signal from input buffer x
-// Writes freq domain signal to output buffer X
+// Input time domain signal in buf
+// Convert to freq domain and store result in buf
 // n (num of samples) must be a power of 2
 // s (stride length) must be 1 for initial function call
-static void fft_recurse(float* x, float complex* X, unsigned int n, unsigned int s) {
-	if (n == 1) {
-		X[0] = x[0] + 0 * I;
-	}
-	else {
-		// Recursively split the DFT in halves
-		fft_recurse(x, X, n/2, s*2);
-		fft_recurse(x+s, X+n/2, n/2, 2*s);
+static void fft_recurse(float complex* buf, unsigned int n, unsigned int s) {
+	if (n == 1) return;
 
-		// Combine halves into full DFT
-		for (unsigned int k = 0; k < n/2; k++) {
-			float complex p = X[k];
-			float complex u = cexpf(I*-2*M_PI*k/n);
-			float complex q = u * X[k+n/2];
-			X[k] = p + q;
-			X[k+n/2] = p - q;
-		}
+	// Recursively split the DFT in halves
+	fft_recurse(buf, n/2, s*2);
+	fft_recurse(buf+n/2, n/2, 2*s);
+
+	// Combine halves into full DFT
+	for (unsigned int k = 0; k < n/2; k++) {
+		float complex p = buf[k];
+		float complex u = cexpf(I*-2*M_PI*k/n);
+		float complex q = u * buf[k+n/2];
+		buf[k] = p + q;
+		buf[k+n/2] = p - q;
 	}
 }
 
-// Wrapper for recursive function call
+// sig_td = input time domain signal (not modified)
+// sig_fd = buffer to store resulting freq domain signal
+// n = sample count (MUST be a power of 2)
 void fft(float* sig_td, float complex* sig_fd, unsigned int n) {
-	// TODO: check params for validity here (not inside fft_recurse)
-	fft_recurse(sig_td, sig_fd, n, 1);
+	// Copy time domain input signal into freq domain output signal
+	// so the FFT can be calculated in-place (better cache coherency)
+	for (unsigned int i = 0; i < n; i++) {
+		sig_fd[i] = sig_td[i] + 0*I;
+	}
+
+	fft_recurse(sig_fd, n, 1);
 }
 
-// Reads freq domain signal from X
-// Writes time domain signal to x
+// Input freq domain signal in buf
+// Convert to time domain and store result in buf
 // n (number of samples) must be a power of 2
 // s (stride length) must be 1 for initial function call
-static void ifft_recurse(float* x, float complex* X, unsigned int n, unsigned int s) {
-	if (n == 1) {
-		x[0] = crealf(X[0]);
+static void ifft_recurse(float complex* buf, unsigned int n, unsigned int s) {
+	if (n == 1) return;
+
+	for (unsigned int k = 0; k < n/2; k++) {
+		float complex u = cexpf(I*-2.f*M_PI*k/n);
+		float complex q = (buf[k] - buf[k+n/2]) / 2.f;
+		float complex p = buf[k] - q;
+		buf[k] = p;
+		buf[k+n/2] = q / u;
 	}
-	else {
-		for (unsigned int k = 0; k < n/2; k++) {
-			float complex u = cexpf(I*-2.f*M_PI*k/n);
-			float complex q = (X[k] - X[k+n/2]) / 2.f;
-			float complex p = X[k] - q;
-			X[k] = p;
-			X[k+n/2] = q / u;
-		}
-		ifft_recurse(x, X, n/2, s*2);
-		ifft_recurse(x+s, X+n/2, n/2, s*2);
-	}
+	ifft_recurse(buf, n/2, s*2);
+	ifft_recurse(buf+n/2, n/2, s*2);
 }
 
-// Wrapper for recursive function call
+// sig_td = buffer to store output time domain signal
+// sig_fd = input freq domain signal (modified due to in-place calculation)
+// n = sample count (MUST be a power of 2)
 void ifft(float* sig_td, float complex* sig_fd, unsigned int n) {
-	// TODO: check params for validity
-	ifft_recurse(sig_td, sig_fd, n, 1);
+	// Calculate FFT in-place in the freq domain signal buffer
+	ifft_recurse(sig_fd, n, 1);
+
+	// Copy real component of freq domain buffer to time domain buffer
+	for (unsigned int i = 0; i < n; i++) {
+		sig_td[i] = crealf(sig_fd[i]);
+	}
 }
